9020934d20f6e3e1e6388cf28ce64b0ccade075b
Mayank00480 <mayanksharma1937@gmail.com>
2023-09-18 20:57:08 +0530

    Second Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('main-header').style.border = '2px solid black';
+//document.title.style.border = '1px solid black';
+document.getElementById('addItemsHeading').style.color = 'green';
+document.getElementById('addItemsHeading').style.fontWeight = 'bold';
+console.log(document.getElementById('addItemsHeading'));
+console.log('hello');
